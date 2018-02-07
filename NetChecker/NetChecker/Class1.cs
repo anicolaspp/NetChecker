4d4147da87c6c4fe9a66795b3e7f78e1d9bcefe8
Nicolas A Perez <anicolaspp@gmail.com>
2018-02-07 17:54:46 -0500

    first commit

---
+++ NetChecker/NetChecker/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace NetChecker
+{
+    public class Class1
+    {
+    }
+
+
+
+    public class Property
+    {
+        public string Name { get; }
+       
+        private Func<bool> predicate { get; }
+
+        public Property(string name, Func<bool> predicate)
+        {
+            Name = name;
+            this.predicate = predicate;
+        }
+
+        public bool Check() => predicate();
+    }
+    
+    
+    
+}
